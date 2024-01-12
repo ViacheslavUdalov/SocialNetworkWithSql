f4590bd3d67401f537d6c2cc73860fb177667a97
Viacheslav Udalov <slava187115@gmail.com>
2024-01-12 18:40:13 +0300

    вывод пользователей на клиент, так же вывод одного пользователя, добавление галереи фотографий.

--- ASP.SecondSocialWithSQL/client/src/app/_services/members.service.ts
+++ ASP.SecondSocialWithSQL/client/src/app/_services/members.service.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@angular/core';
 import {enviroment} from "../../enviroments/enviroment";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 import {Member} from "../_models/member";
 
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorizations: "Bearer " + JSON.parse(localStorage.getItem('user')).token
-  })
-}
+
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
 baseUrl = enviroment.apiUrl;
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+}
+
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users' + username, httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
