79c0e47727019166d94a30773bc9e4daeb829d4f
Viacheslav Udalov <slava187115@gmail.com>
2024-01-12 21:00:21 +0300

    редактирование пользователя, исппользование loading, так же нет загрузки с сервера, когда пользователь открывает тот же роутинг, который уже был открыт. Добавление интерсептора.

--- ASP.SecondSocialWithSQL/Controllers/UserController.cs
+++ ASP.SecondSocialWithSQL/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using ASP.SecondSocialWithSQL.DTOS;
 using ASP.SecondSocialWithSQL.Entities;
 using ASP.SecondSocialWithSQL.Interfaces;
@@ -30,5 +31,16 @@ public class UsersController : BaseApiController
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
      return await _userRepository.GetMemberAsync(username);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+        _mapper.Map(memberUpdateDto, user);
+        _userRepository.Update(user);
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update user.");
     }
 }
